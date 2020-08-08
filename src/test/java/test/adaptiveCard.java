69aadb7d22fca2967b87edd2fc81c6238749bbd5
nipuni.gunatilake <nipuni.gunatilake@rakuten.com>
2020-08-08 16:52:36 +0900

    modified file  Please enter the commit message for your changes. Lines starting

--- src/test/java/test/adaptiveCard.java
+++ src/test/java/test/adaptiveCard.java
@@ -1,54 +1,47 @@
 package test;
 
-
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.android.AndroidKeyCode;
+import functions.login;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import page.base;
 import dataProvider.dataProvider;
+
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
-public class adaptiveCard extends base {
 
-    AndroidDriver<AndroidElement> driver= capabilities();
+public class adaptiveCard extends login{
+
+
+
+
+
+    SoftAssert softAssert = new SoftAssert();
 
     public adaptiveCard() throws MalformedURLException {
     }
-    SoftAssert softAssert = new SoftAssert();
 
 
     @BeforeClass(description = "Load the Device", alwaysRun = true)
-    public void Login () {
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElementById(login()).click();
-        driver.findElementById(getPermission()).click();
+    public void LoginApp () {
+        login();
 
     }
 
     @Test(description = "verify adaptiveCard", alwaysRun = true, priority = 1)
-    public void selectAdaptiveCard() {
+    public void VerifySelectAdaptiveCard() {
 
-        driver.findElementByAndroidUIAutomator(btnAdaptiveCard()).click();
-        driver.findElementByClassName(dropDown()).click();
-        driver.findElementByAndroidUIAutomator(txtDropDown()).click();
-        driver.findElementByAndroidUIAutomator(value()).click();
-        driver.pressKeyCode(AndroidKeyCode.DEL);
-        driver.pressKeyCode(AndroidKeyCode.KEYCODE_3);
-        driver.findElementByAndroidUIAutomator(btnCalculate()).click();
-        String value=driver.findElementByAndroidUIAutomator( valueText()).getText();
+        selectAdaptiveCard();
+        String value=getAdaptiveCardValue();
         softAssert.assertEquals(value,dataProvider.card_Value());
         softAssert.assertAll();
 
     }
 
     @AfterClass(description = "Quit Device", alwaysRun = true)
+
     public void quit() {
-        driver.quit();
+
+        logout();
     }
 }
