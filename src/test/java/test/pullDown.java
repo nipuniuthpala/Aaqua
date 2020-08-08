e76bc0f5c7dd821fa386df62abbed15abee5fcc3
nipuni.gunatilake <nipuni.gunatilake@rakuten.com>
2020-08-08 16:59:08 +0900

    modified# Please enter the commit message for your changes. Lines starting

--- src/test/java/test/pullDown.java
+++ src/test/java/test/pullDown.java
@@ -1,45 +1,42 @@
 package test;
 
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
+import functions.login;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import page.base;
 import dataProvider.dataProvider;
+
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
-public class pullDown extends base {
 
-    AndroidDriver<AndroidElement> driver= capabilities();
+public class pullDown extends login {
+
+
+
+    SoftAssert softAssert = new SoftAssert();
+
 
     public pullDown() throws MalformedURLException {
     }
-    SoftAssert softAssert = new SoftAssert();
-
 
     @BeforeClass(description = "Load the Device", alwaysRun = true)
     public void Login () {
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElementById(login()).click();
-        driver.findElementById(getPermission()).click();
+       login();
     }
 
     @Test(description = "verify PullDown", alwaysRun = true, priority = 1)
     public void selectPullDown() {
 
-        driver.findElementByAndroidUIAutomator(btnPullDown()).click();
-        driver.findElementByClassName(dropDown()).click();
-        driver.findElementByAndroidUIAutomator(btnOption()).click();
-        driver.findElementByAndroidUIAutomator(btnSubmit()).click();
-        String value=driver.findElementByAndroidUIAutomator(btnOption()).getText();
+        clickPullDown();
+        String value=getBtnOptionText();
         softAssert.assertEquals(value,dataProvider.option());
-        driver.findElementByAndroidUIAutomator(backToTop()).click();
-        boolean markdown=driver.findElementByAndroidUIAutomator(markdown()).isDisplayed();
+        clickBackToTop();
+        displayCarousal();
+        navigateToMarkdown();
+        boolean markdown=markdownIsEnabled();
         softAssert.assertTrue(markdown);
         softAssert.assertAll();
 
@@ -48,6 +45,6 @@ public class pullDown extends base {
 
     @AfterClass(description = "Quit Device", alwaysRun = true)
     public void quit() {
-        driver.quit();
+        logout();
     }
 }
