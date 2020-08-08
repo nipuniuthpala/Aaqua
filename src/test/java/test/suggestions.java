e0ff3d069a6ea076bc00431f6e1095e57bccf563
nipuni.gunatilake <nipuni.gunatilake@rakuten.com>
2020-08-08 17:01:26 +0900

    modified# Please enter the commit message for your changes. Lines starting

--- src/test/java/test/suggestions.java
+++ src/test/java/test/suggestions.java
@@ -1,40 +1,33 @@
 package test;
 
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
+import functions.login;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import page.base;
 import dataProvider.dataProvider;
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
-public class suggestions extends base {
+public class suggestions extends login {
 
-    AndroidDriver<AndroidElement> driver= capabilities();
+
+    SoftAssert softAssert = new SoftAssert();
 
     public suggestions() throws MalformedURLException {
     }
-    SoftAssert softAssert = new SoftAssert();
 
 
     @BeforeClass(description = "Load the Device", alwaysRun = true)
     public void Login () {
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElementById(login()).click();
-        driver.findElementById(getPermission()).click();
+      login();
     }
 
     @Test(description = "verify suggestions", alwaysRun = true, priority = 1)
     public void verifySuggestions() {
-        driver.findElementById(textArea()).sendKeys(dataProvider.input());
-        driver.findElementById(btnSuggestion()).click();
-        driver.findElementById(btnEnter()).click();
-        String text=  driver.findElementByAndroidUIAutomator(txtSuggestion()).getText();
+        enterSuggestions();
+        String text=getSuggestionsText();
         softAssert.assertEquals(text,dataProvider.outputTxt());
         softAssert.assertAll();
 
@@ -42,7 +35,8 @@ public class suggestions extends base {
 
     @AfterClass(description = "Quit Device", alwaysRun = true)
     public void quit() {
-        driver.quit();
+        logout();
     }
+
 
 }
