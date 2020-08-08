c82ff38e59f5cd5e92c5fb5dc9aadc3014551d17
nipuni.gunatilake <nipuni.gunatilake@rakuten.com>
2020-08-08 17:01:01 +0900

    modified# Please enter the commit message for your changes. Lines starting

--- src/test/java/test/inputDisable.java
+++ src/test/java/test/inputDisable.java
@@ -1,52 +1,50 @@
 package test;
 
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
+import functions.login;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import page.base;
 import dataProvider.dataProvider;
+
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
-public class inputDisable extends base {
 
-    AndroidDriver<AndroidElement> driver= capabilities();
+public class inputDisable extends login {
+
+
+
+    SoftAssert softAssert = new SoftAssert();
 
     public inputDisable() throws MalformedURLException {
     }
-    SoftAssert softAssert = new SoftAssert();
 
 
     @BeforeClass(description = "Load the Device", alwaysRun = true)
     public void Login () {
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElementById(login()).click();
-        driver.findElementById(getPermission()).click();
+       login();
     }
 
     @Test(description = "verify inputDisable", alwaysRun = true, priority = 1)
     public void selectInputDisable() {
 
-        driver.findElementByAndroidUIAutomator(btnInputDisable()).click();
-        boolean text=driver.findElementById(textArea()).isEnabled();
+        clickInputDisable();
+        boolean text=TextIsEnabled();
         softAssert.assertFalse(text);
-        driver.findElementById(btnMinimize()).click();
-        driver.findElementById(login()).click();
-        boolean text1=driver.findElementById(textArea()).isEnabled();
+        minimize();
+        clickBtnLoginIcon();
+        boolean text1=TextIsEnabled();
         softAssert.assertFalse(text1);
-        String text3=driver.findElementByAndroidUIAutomator(txtInputDisable()).getText();
+        String text3=getInputDisableText();
         softAssert.assertEquals(text3,dataProvider.inputText());
         softAssert.assertAll();
 
     }
     @AfterClass(description = "Quit Device", alwaysRun = true)
     public void quit() {
-        driver.quit();
+        logout();
     }
 
     }
