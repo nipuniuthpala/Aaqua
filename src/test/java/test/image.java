c73349bc368fc27c991f2ad85e77549e986796eb
nipuni.gunatilake <nipuni.gunatilake@rakuten.com>
2020-08-08 16:54:42 +0900

    modified

--- src/test/java/test/image.java
+++ src/test/java/test/image.java
@@ -1,38 +1,34 @@
 package test;
 
-
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
+import functions.login;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import page.base;
+
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
-public class image extends base {
 
-    AndroidDriver<AndroidElement> driver= capabilities();
+public class image extends login {
+
+
+    SoftAssert softAssert = new SoftAssert();
 
     public image() throws MalformedURLException {
     }
-    SoftAssert softAssert = new SoftAssert();
 
 
     @BeforeClass(description = "Load the Device", alwaysRun = true)
     public void Login () {
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElementById(login()).click();
-        driver.findElementById(getPermission()).click();
+        login();
     }
 
     @Test(description = "verify image", alwaysRun = true, priority = 1)
     public void selectImage() {
 
-        driver.findElementByAndroidUIAutomator(btnImage()).click();
-        boolean image=driver.findElementByClassName(viewImage()).isDisplayed();
+        clickImage();
+        boolean image=verifyImage();
         softAssert.assertTrue(image);
         softAssert.assertAll();
 
@@ -40,6 +36,6 @@ public class image extends base {
 
     @AfterClass(description = "Quit Device", alwaysRun = true)
     public void quit() {
-        driver.quit();
+        logout();
     }
 }
