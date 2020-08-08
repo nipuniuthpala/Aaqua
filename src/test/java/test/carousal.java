79258e8a4c818356e263734303e42f4ddd8e0f88
nipuni.gunatilake <nipuni.gunatilake@rakuten.com>
2020-08-08 16:53:34 +0900

    modified

--- src/test/java/test/carousal.java
+++ src/test/java/test/carousal.java
@@ -1,56 +1,40 @@
 package test;
 
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.android.AndroidKeyCode;
+import functions.login;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import page.base;
+
 import java.net.MalformedURLException;
-import java.util.concurrent.TimeUnit;
 
 
+public class carousal extends login {
 
-public class carousal extends base {
 
-    AndroidDriver<AndroidElement> driver= capabilities();
-
-    public carousal() throws MalformedURLException {
+    public carousal()throws MalformedURLException {
     }
+
     SoftAssert softAssert = new SoftAssert();
-    TouchAction y =new TouchAction(driver);
+
 
     @BeforeClass(description = "Load the Device", alwaysRun = true)
     public void Login () {
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElementById(login()).click();
-        driver.findElementById(getPermission()).click();
+      login();
 
     }
 
     @Test(description = "verify select carousal and check link", alwaysRun = true, priority = 1)
-    public void selectCarousal() {
-
-        driver.findElementByAndroidUIAutomator(btnCarousal()).click();
-        driver.findElementByAndroidUIAutomator(btnMoreDetails()).click();
-        driver.pressKeyCode(AndroidKeyCode.BACK);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        y.press( 1249, 1388).moveTo( 305, 1399).release().perform();
-        driver.findElementByAndroidUIAutomator(btnMoreDetails()).click();
-        driver.pressKeyCode(AndroidKeyCode.BACK);
-        driver.findElementByAndroidUIAutomator(btnReserve()).click();
-        driver.pressKeyCode(AndroidKeyCode.BACK);
+    public void selectCarousalTest() {
+        selectCarousal();
         softAssert.assertAll();
-
     }
 
 
     @AfterClass(description = "Quit Device", alwaysRun = true)
     public void quit() {
-        driver.quit();
+
+        logout();
     }
 }
